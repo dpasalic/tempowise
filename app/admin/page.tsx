c1b1d2c87032e4a39e439839693548b2b643b43f
dpasalic <pasalic.dev@gmail.com>
2024-09-04 21:42:38 +0200

    v2.1

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -25,22 +25,22 @@ export default async function AdminPanel() {
   });
 
   return (
-    <main className="px-24 pt-24 pb-32">
+    <main className="px-8 md:px-24 pt-24 pb-32">
       <h1 className="pb-12 text-4xl font-black">Admin Panel</h1>
-      <div className="flex gap-8">
-        <section className="grow flex justify-center items-center gap-24 p-8 border border-zinc-500 rounded-sm">
+      <div className="flex flex-col lg:flex-row gap-8">
+        <section className="grow flex flex-col 2xl:flex-row justify-center items-center gap-24 p-8 border border-zinc-500 rounded-sm">
           <div className="flex flex-col items-center">
-            <h3 className="pb-4 text-9xl font-black">{overall.total_tasks}</h3>
+            <h3 className="pb-4 text-8xl font-black">{overall.total_tasks}</h3>
             <div className="flex items-center gap-2">
               <LeftToRightListNumberIcon width={50} height={50} color="#71717a" />
               <p className="text-lg text-zinc-500">Total number <br />of tasks</p>
             </div>
           </div>
-          <div className="w-[500px] h-96">
+          <div className="w-64 md:w-[500px] h-64 md:h-96">
             <TasksChart data={chartData} />
           </div>
           <div className="flex flex-col items-center">
-            <h3 className="pb-4 text-9xl font-black">{convertMinutesToHM(overall.total_minutes)}</h3>
+            <h3 className="pb-4 text-8xl font-black">{convertMinutesToHM(overall.total_minutes)}</h3>
             <div className="flex items-center gap-2">
               <HourglassIcon width={50} height={50} color="#71717a" />
               <p className="text-lg text-zinc-500">Total time<br />tasks require</p>
@@ -48,7 +48,7 @@ export default async function AdminPanel() {
           </div>
         </section>
         <section className="grow flex flex-col justify-center items-center p-8 border border-zinc-500 rounded-sm">
-          <h3 className="pb-4 text-9xl font-black">{usersData.length}</h3>
+          <h3 className="pb-4 text-8xl font-black">{usersData.length}</h3>
           <div className="flex items-center gap-2">
             <LeftToRightListNumberIcon width={50} height={50} color="#71717a" />
             <p className="text-lg text-zinc-500">Total number<br />of users</p>
